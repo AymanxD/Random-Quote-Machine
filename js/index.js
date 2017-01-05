d2291da91741252868c98d43cb9005fbb239e331
Ayman Mohatarem <aymanmohatarem@gmail.com>
2017-01-05 11:09:51 -0500

    Comments

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   var randomQuote, randomAuthor;
    var quotewebsite = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
    findquote();  // At the start in order to create starter quote.
+  
+ // Using the Forismatic API
  function findquote(){
   $.getJSON(quotewebsite, function(quote) {
     randomQuote = quote.quoteText;
@@ -14,6 +16,7 @@ $(document).ready(function() {
  };
     $("#Newquotebutton").click((e) => findquote());  
 
+  // Opens twitter window in order to tweet the quote
   $("#Tweet").on("click", function() {
       window.open("https://twitter.com/intent/tweet/?text=" + randomQuote + " " +randomAuthor);             
     }); 
