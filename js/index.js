4a70369f1d92013e7e0aca1cf339b7915cf9ed8a
AymanxD <aymanmohatarem@gmail.com>
2017-01-18 13:02:00 -0500

    Extended commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var randomQuote, randomAuthor;
    var quotewebsite = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
    findquote();  // At the start in order to create starter quote.
-  
+
  // Using the Forismatic API
  function findquote(){
   $.getJSON(quotewebsite, function(quote) {
@@ -11,13 +11,12 @@ $(document).ready(function() {
     randomAuthor = quote.quoteAuthor;
   $(".Quotes").text(randomQuote);
   $(".Author").text(randomAuthor);
-    console.log(quote)
   });
  };
-    $("#Newquotebutton").click((e) => findquote());  
+  $("#Newquotebutton").click((e) => findquote());
 
   // Opens twitter window in order to tweet the quote
   $("#Tweet").on("click", function() {
-      window.open("https://twitter.com/intent/tweet/?text=" + randomQuote + " " +randomAuthor);             
-    }); 
+      window.open("https://twitter.com/intent/tweet/?text=" + randomQuote + " " +randomAuthor);
+    });
 });
